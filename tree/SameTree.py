6459dcafddc88475796206e3276e8950457ce577
feiyang.liu <feiyang.liu@11bee.com>
2020-02-03 21:07:02 +0800

    判断相同的树，递归方法

---
+++ tree/SameTree.py
@@ -0,0 +1,35 @@
+'''
+给定两个二叉树，编写一个函数来检验它们是否相同。
+
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ class TreeNode:
+    def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+'''
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+
+
+class SameTree:
+    def isSameTree(self, p:TreeNode, q:TreeNode)->bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+
+
+
